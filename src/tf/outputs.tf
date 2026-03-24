354946d091d64ef35f494c636c1e4db6c4d23872
Levi Zitting <me@levizitting.com>
2026-03-24 06:33:33 -0500

    feat: provision Flux git key and status token paths

--- src/tf/outputs.tf
+++ src/tf/outputs.tf
@@ -10,3 +10,23 @@ output "ssh_private_key_ssm_path" {
   description = "SSM path for generated workload SSH private key"
   value       = module.ssh_key.ssm_path
 }
+
+output "flux_git_private_key_ssm_path" {
+  description = "SSM path for Flux Git deploy private key"
+  value       = module.flux_deploy_key.ssm_path
+}
+
+output "flux_git_public_key" {
+  description = "Flux Git deploy public key to add as repository deploy key"
+  value       = module.flux_deploy_key.public_key
+}
+
+output "flux_git_public_key_ssm_path" {
+  description = "SSM path for Flux Git deploy public key"
+  value       = local.flux_git_public_key_path
+}
+
+output "flux_github_status_token_ssm_path" {
+  description = "SSM path for Flux GitHub status token"
+  value       = aws_ssm_parameter.github_status_token.name
+}
--- src/tf/ssh_key_inventory.tf
+++ src/tf/ssh_key_inventory.tf
@@ -2,6 +2,11 @@ locals {
   vm_ssh_key_name             = "infra-vm-workloads"
   vm_ssh_key_version          = 1
   vm_ssh_private_key_ssm_path = "/vm-workloads/sgfdevs/infra-vm-workloads/ssh-private-key"
+  flux_key_name               = "flux-infra-vm-workloads"
+  flux_key_version            = 1
+  flux_git_private_key_path   = "/vm-workloads/sgfdevs/infra-vm-workloads/flux-git-private-key"
+  flux_git_public_key_path    = "/vm-workloads/sgfdevs/infra-vm-workloads/flux-git-public-key"
+  github_status_token_path    = "/vm-workloads/sgfdevs/infra-vm-workloads/flux-github-status-token"
 }
 
 module "ssh_key" {
@@ -9,6 +14,21 @@ module "ssh_key" {
   name                 = local.vm_ssh_key_name
   key_version          = local.vm_ssh_key_version
   ssm_private_key_path = local.vm_ssh_private_key_ssm_path
+}
+
+module "flux_deploy_key" {
+  source               = "git::https://github.com/glitchedmob/infra-shared.git//src/tf/modules/ssh-key?ref=main"
+  name                 = local.flux_key_name
+  key_version          = local.flux_key_version
+  ssm_private_key_path = local.flux_git_private_key_path
+  ssm_public_key_path  = local.flux_git_public_key_path
+}
+
+resource "aws_ssm_parameter" "github_status_token" {
+  name             = local.github_status_token_path
+  type             = "SecureString"
+  value_wo         = "CHANGEME"
+  value_wo_version = 1
 }
 
 resource "ansible_group" "k3s_servers" {
@@ -34,11 +54,13 @@ resource "ansible_host" "workload" {
   ]
 
   variables = {
-    ansible_host              = each.value.ipv4_address
-    ansible_user              = local.vm_user
-    node_name                 = each.value.node_name
-    ssm_private_key_path      = module.ssh_key.ssm_path
-    proxmox_vm_role           = each.value.role
-    ansible_ssh_use_ssh_agent = "false"
+    ansible_host                  = each.value.ipv4_address
+    ansible_user                  = local.vm_user
+    node_name                     = each.value.node_name
+    ssm_private_key_path          = module.ssh_key.ssm_path
+    ssm_flux_git_private_key_path = module.flux_deploy_key.ssm_path
+    ssm_github_status_token_path  = aws_ssm_parameter.github_status_token.name
+    proxmox_vm_role               = each.value.role
+    ansible_ssh_use_ssh_agent     = "false"
   }
 }
