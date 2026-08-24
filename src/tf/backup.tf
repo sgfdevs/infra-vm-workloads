084cb7f8f830216d9c2b5673d483bf698d324c55
Levi Zitting <me@levizitting.com>
2026-08-24 08:24:40 -0500

    feat: add Zitadel secrets

--- src/tf/backup.tf
+++ src/tf/backup.tf
@@ -1,7 +1,7 @@
 locals {
   backup_bucket_name = "sgfdevs-on-prem-k3s-backups"
   backup_ssm_prefix  = "/vm-workloads/sgfdevs/infra-vm-workloads/backups"
-  backup_apps        = toset(["openbao"])
+  backup_apps        = toset(["openbao", "zitadel"])
 }
 
 resource "b2_bucket" "backups" {
